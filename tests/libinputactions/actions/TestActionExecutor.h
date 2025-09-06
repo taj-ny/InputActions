ec7fdb72eeff10dc2c3bce3c8afedef969d9ad85
taj-ny <m@rcin.dev>
2025-09-06 15:40:25 +0200

    tests: add ActionExecutor tests

---
+++ tests/libinputactions/actions/TestActionExecutor.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <QTest>
+#include <libinputactions/actions/ActionExecutor.h>
+
+namespace libinputactions
+{
+
+class TestActionExecutor : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void init();
+
+    void execute_syncAction_executedOnMainThread();
+    void execute_asyncAction_executedOnActionThread();
+    void execute_syncActionWhileActionThreadIsBusy_executedOnActionThread();
+    void execute_syncAndAsyncActions_orderPreserved();
+
+private:
+    static bool isMainThread();
+
+    std::unique_ptr<ActionExecutor> m_executor;
+};
+
+}
