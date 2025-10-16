da24e797411523969826f2b84a18f8fb3a29d14e
taj-ny <m@rcin.dev>
2025-10-16 12:07:12 +0200

    libinputactions: fix execution count of async actions

--- tests/libinputactions/actions/TestAction.cpp
+++ tests/libinputactions/actions/TestAction.cpp
@@ -1,32 +1,33 @@
 #include "TestAction.h"
 #include <libinputactions/actions/Action.h>
+#include <libinputactions/actions/ActionExecutor.h>
 
 namespace libinputactions
 {
 
 void TestAction::init()
 {
-    m_action = {};
+    m_action = std::make_shared<Action>();
 }
 
 void TestAction::canExecute_noLimit()
 {
-    QVERIFY(m_action.canExecute());
-    m_action.execute();
-    QVERIFY(m_action.canExecute());
+    QVERIFY(m_action->canExecute());
+    g_actionExecutor->execute(m_action);
+    QVERIFY(m_action->canExecute());
 }
 
 void TestAction::canExecute_withLimit()
 {
-    m_action.m_executionLimit = 1;
-    QVERIFY(m_action.canExecute());
-    m_action.execute();
-    QVERIFY(!m_action.canExecute());
+    m_action->m_executionLimit = 1;
+    QVERIFY(m_action->canExecute());
+    g_actionExecutor->execute(m_action);
+    QVERIFY(!m_action->canExecute());
 
-    m_action.reset();
-    QVERIFY(m_action.canExecute());
-    m_action.execute();
-    QVERIFY(!m_action.canExecute());
+    m_action->reset();
+    QVERIFY(m_action->canExecute());
+    g_actionExecutor->execute(m_action);
+    QVERIFY(!m_action->canExecute());
 }
 
 }
