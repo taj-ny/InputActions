e33bd6ef44e5c506ba9261e7324f0609bf48be1d
taj_ny <m@rcin.dev>
2026-02-08 22:02:05 +0100

    fix tests

--- tests/libinputactions/handlers/TestInputTriggerHandler.cpp
+++ tests/libinputactions/handlers/TestInputTriggerHandler.cpp
@@ -1,7 +1,6 @@
 #include "TestInputTriggerHandler.h"
 #include <QSignalSpy>
-#include <libinputactions/handlers/InputTriggerHandler.h>
-#include <libinputactions/input/devices/InputDevice.h>
+#include <libinputactions/input/backends/InputBackend.h>
 #include <libinputactions/input/events.h>
 #include <libinputactions/triggers/Trigger.h>
 #include <linux/input-event-codes.h>
@@ -9,20 +8,46 @@
 namespace InputActions
 {
 
-void TestInputTriggerHandler::keyboardKey()
+void TestInputTriggerHandler::init()
 {
-    InputDevice device(InputDeviceType::Keyboard);
-    InputTriggerHandler handler;
-    handler.setDevice(&device);
-    QSignalSpy spy(&handler, &TriggerHandler::endingTriggers);
-    handler.addTrigger(std::make_unique<Trigger>(TriggerType::Press));
+    m_handler = std::unique_ptr<InputTriggerHandler>(new InputTriggerHandler);
 
-    handler.activateTriggers(TriggerType::Press);
-    handler.handleEvent(KeyboardKeyEvent(&device, KEY_LEFTCTRL, true));
+    g_inputBackend = std::make_unique<InputBackend>();
+    g_inputBackend->initialize();
+
+    m_keyboard = std::make_unique<InputDevice>(InputDeviceType::Keyboard);
+    g_inputBackend->addDevice(m_keyboard.get());
+}
+
+void TestInputTriggerHandler::keyboardKey__modifierReleased_pressedBeforeTriggerActivation__triggersEnded()
+{
+    QSignalSpy spy(m_handler.get(), &TriggerHandler::endingTriggers);
+    m_handler->addTrigger(std::make_unique<Trigger>(TriggerType::Press));
+
+    g_inputBackend->handleEvent(KeyboardKeyEvent(m_keyboard.get(), KEY_LEFTCTRL, true));
+    m_handler->handleEvent(KeyboardKeyEvent(m_keyboard.get(), KEY_LEFTCTRL, true));
+
+    m_handler->activateTriggers(TriggerType::Press);
     QCOMPARE(spy.count(), 0);
 
-    handler.handleEvent(KeyboardKeyEvent(&device, KEY_LEFTCTRL, false));
+    g_inputBackend->handleEvent(KeyboardKeyEvent(m_keyboard.get(), KEY_LEFTCTRL, false));
+    m_handler->handleEvent(KeyboardKeyEvent(m_keyboard.get(), KEY_LEFTCTRL, false));
     QCOMPARE(spy.count(), 1);
+}
+
+void TestInputTriggerHandler::keyboardKey__modifierReleased_pressedAfterTriggerActivation__triggersNotEnded()
+{
+    QSignalSpy spy(m_handler.get(), &TriggerHandler::endingTriggers);
+    m_handler->addTrigger(std::make_unique<Trigger>(TriggerType::Press));
+
+    m_handler->activateTriggers(TriggerType::Press);
+
+    g_inputBackend->handleEvent(KeyboardKeyEvent(m_keyboard.get(), KEY_LEFTCTRL, true));
+    m_handler->handleEvent(KeyboardKeyEvent(m_keyboard.get(), KEY_LEFTCTRL, true));
+
+    g_inputBackend->handleEvent(KeyboardKeyEvent(m_keyboard.get(), KEY_LEFTCTRL, false));
+    m_handler->handleEvent(KeyboardKeyEvent(m_keyboard.get(), KEY_LEFTCTRL, false));
+    QCOMPARE(spy.count(), 0);
 }
 
 }
